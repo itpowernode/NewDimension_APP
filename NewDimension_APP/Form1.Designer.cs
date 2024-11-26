065676afd630fb871891ec116400698f08daf9c9
anthoncylam <90413265+anthoncylam@users.noreply.github.com>
2024-11-26 14:13:34 -0500

    udpate commit

--- NewDimension_APP/Form1.Designer.cs
+++ NewDimension_APP/Form1.Designer.cs
@@ -498,7 +498,7 @@
             this.MinimumSize = new System.Drawing.Size(820, 450);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "NEW DIMENSION v1.01";
+            this.Text = "NEW DIMENSION v1.03";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl_home.ResumeLayout(false);
